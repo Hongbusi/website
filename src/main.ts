2ae0d67fffeca21d2510ec8a6a5a10310bd5c66f
Hongbusi <q951686662@qq.com>
2021-11-26 18:01:40 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 // import './utils/debug.js';
 import { createApp } from 'vue';
+import Router from './router';
 import App from './App.vue';
 import './styles/index.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(Router);
+app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA()
-  ]
+  plugins: [vue(), VitePWA()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
